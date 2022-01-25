cfe84674b8b1699671137245b00d87c5cbeeb330
mahadevananair1 <mahadevananair@gmail.com>
2022-01-25 20:08:33 +0530

    added condition to JSX code so that when no expeness are present a p tag is displayed saying: no items to showcase, this is done using javascript turnary operator which behaves really wierd because if condition is true the whatever comes after && is returned 😂😂 check code in line 22 of ExpenseItemGroup.js

--- react-adventure-1/src/components/ExpenseTiles/ExpenseItemGroup.js
+++ react-adventure-1/src/components/ExpenseTiles/ExpenseItemGroup.js
@@ -19,27 +19,14 @@ const filteredExpenses = props.expenses.filter((expense) => expense.date.getFull
     <div className="expenses">
     <ExpensesFilter selected={filteredYear} ChangeDate = {expensesFilterDateHandler}/>
       
-      {filteredExpenses.map((expense) => (<ExpenseItem key={expense.id} title = {expense.title} amount={expense.amount} date={expense.date}/> ))}
-      {/* <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      /> */}
+      {filteredExpenses.length === 0 && <p>No items to showcase in this year</p>}
+
+      { 
+        filteredExpenses.length > 0 && filteredExpenses.map((expense) => (<ExpenseItem key={expense.id} title = {expense.title} amount={expense.amount} date={expense.date}/> )) 
+      }
+
+
+     
     </div>
   );
 }
